3241358f5ec550a406313e10434b66cd19286ddf
Prasad P Shetty <60071398+prasadpshetty@users.noreply.github.com>
2022-07-08 12:53:34 +0530

    Apartment_hunting-solution -python code

---
+++ Solution_code.py
@@ -0,0 +1,18 @@
+def apartmentHunting(blocks,reqs):
+    avail=[[index for index,block in enumerate(blocks)if block[building]] for building in reqs]
+    distance=[max(min((abs(i-ele)) for ele in item) for item in avail) for i in range(len(blocks))]
+    optimumblock=distance.index(min(distance))
+    maxdistance=distance[optimumblock]
+    return optimumblock,maxdistance
+
+blocks=[{'gym': False, 'office': False, 'school': True, 'store': False}, 
+        {'gym': True, 'office': True, 'school': False, 'store': False}, 
+        {'gym': False, 'office': False, 'school': True, 'store': False}, 
+        {'gym': False, 'office': False, 'school': True, 'store': False}, 
+        {'gym': True, 'office': True, 'school': True, 'store': False}, 
+        {'gym': True, 'office': True, 'school': False, 'store': False}, 
+        {'gym': False, 'office': False, 'school': False, 'store': True}]
+reqs=['gym', 'office', 'school', 'store']
+
+optimumblock,maxdistance=apartmentHunting(blocks,reqs)
+print('{}, At block with index {}, the farthest you would have to walk to reach any of the required building is {}; at any other index, you would have to walk farther.'.format(optimumblock,optimumblock,maxdistance))
